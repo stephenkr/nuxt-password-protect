540384165ab6b07f6d367e9e8f9fdbe0d3e73ddf
Stephen Kerr <stephenkerr0312@gmail.com>
2019-08-10 16:58:48 +0200

    feat: Setup example

---
+++ examples/demo/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Demo</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ['passwordProtect']
+}
+</script>
---
+++ examples/demo/pages/password.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Password?</h1>
+    <p v-if="isAuthorised">
+      Is logged in, <a @click="removeAuthorisation">Log out</a>
+    </p>
+    <a v-else @click="verifyUser">Click here</a>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    isAuthorised() {
+      return this.$passwordProtect.isAuthorised()
+    }
+  },
+
+  methods: {
+    verifyUser() {
+      this.$passwordProtect.authorise('password')
+    },
+    removeAuthorisation() {
+      this.$passwordProtect.removeAuthorisation()
+    }
+  }
+}
+</script>
