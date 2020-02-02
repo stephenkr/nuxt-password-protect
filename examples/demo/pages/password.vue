1fdfccefad4d65e58423794b7eb0461347080ab5
Stephen <stephenkerr0312@gmail.com>
2020-02-02 16:31:11 +0100

    docs: Improve example site

--- examples/demo/pages/password.vue
+++ examples/demo/pages/password.vue
@@ -1,47 +1,58 @@
 <template>
   <div class="container">
-    <h1>Password?</h1>
-    <p>Enter in your password, you can do so by filling in the form below.</p>
-    <p v-if="!isAuthorised">
-      You are not logged in
-    </p>
-    <a class="button--pink" href="/">Go to password protected area</a>
+    <div v-if="isAuthorised">
+      <h1>Looks like you're already logged in</h1>
 
-    <div class="section">
-      <h2>Log in using a form</h2>
+      <p>Either logout or go to the password protected area</p>
 
-      <form method="GET" action="/">
-        <input value="pass" type="password" name="_pw" placeholder="Your password">
-        <button class="button--pink" type="submit">
-          Log in
-        </button>
-      </form>
+      <a class="button--pink" @click="removeAuthorisation">Log out</a>
+      <a class="button--pink" href="/">Go to password protected area</a>
     </div>
-    <div class="section">
-      <h2>Log in using a Vue method</h2>
+    <div v-else>
+      <h1>Log in</h1>
+      <p>Enter in your password, you can do so by filling in the form below.</p>
 
-      <p>Using this method will login the user in if the correct password is passed using the nuxt-password-protect authorise method.</p>
+      <div class="section">
+        <h2>Log in using a form</h2>
 
-      <a class="button--pink" @click="loginUser()">Login using a method</a>
+        <form method="GET" action="/">
+          <input value="pass" type="password" name="_pw" placeholder="Your password">
+          <button class="button--pink" type="submit">
+            Log in
+          </button>
+        </form>
+      </div>
+      <div class="section">
+        <h2>Log in using a Vue method</h2>
+
+        <p>Using this method will login the user in if the correct password is passed using the nuxt-password-protect authorise method.</p>
+
+        <a class="button--pink" @click="loginUser()">Login using a method</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    isAuthorised() {
-      return this.$passwordProtect.isAuthorised()
+  data: function() {
+    return {
+      isAuthorised: false
     }
+  },
+  mounted() {
+    this.isAuthorised = this.$passwordProtect.isAuthorised()
   },
 
   methods: {
     loginUser() {
       this.$passwordProtect.authorise('pass')
+      this.isAuthorised = this.$passwordProtect.isAuthorised()
       this.$router.push('/')
     },
     removeAuthorisation() {
       this.$passwordProtect.removeAuthorisation()
+      this.isAuthorised = this.$passwordProtect.isAuthorised()
     }
   }
 }
