06a17ba66540291b7b2887ff9a240fa2b1046a8e
Stephen <stephenkerr0312@gmail.com>
2021-02-12 16:48:56 +0100

    refactor: Update example with dynamic page

---
+++ examples/demo/pages/_id.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <h1>This is a dynamic page</h1>
+    <p>This area is the password protected</p>
+
+    <a @click="logout()" class="button--pink">Log out</a>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: ['password-protect'],
+  methods: {
+    logout() {
+      this.$passwordProtect.removeAuthorisation()
+      this.$router.push('/password')
+    }
+  }
+}
+</script>
+
+<style scoped>
+p + .button--pink {
+  margin-top: 10px;
+}
+</style>
